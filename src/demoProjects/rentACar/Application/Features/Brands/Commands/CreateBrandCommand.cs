6ade64d842a348c760dbb5bc1759fa1f7814b781
baran üçdağ <baran.ucdag@gazi.edu.tr>
2022-08-27 14:26:55 +0300

    Add project files.

---
+++ src/demoProjects/rentACar/Application/Features/Brands/Commands/CreateBrandCommand.cs
@@ -0,0 +1,33 @@
+﻿using Application.Features.Brands.Dtos;
+using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Features.Brands.Commands
+{
+    public class CreateBrandCommand : IRequest<CreateBrandDto>
+    {
+        public string Name { get; set; }
+
+        public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreateBrandDto>
+        {
+            private readonly IBrandRepository brandRepository;
+            private readonly IMapper mapper;
+
+            public CreateBrandCommandHandler(IBrandRepository brandRepository, IMapper mapper)
+            {
+                this.brandRepository = brandRepository;
+                this.mapper = mapper;
+            }
+
+            public async Task<CreateBrandDto> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+            {
+                Brand mappedBrand= mapper.Map<Brand>(request);
+                Brand createdBrand = await brandRepository.AddAsync(mappedBrand);
+                CreateBrandDto createdBrandDto = mapper.Map<CreateBrandDto>(createdBrand);
+                return createdBrandDto;
+            }
+        }
+    }
+}
---
+++ src/demoProjects/rentACar/Application/Features/Brands/Profiles/MappingProfiles.cs
@@ -0,0 +1,16 @@
+﻿using Application.Features.Brands.Commands;
+using Application.Features.Brands.Dtos;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Features.Brands.Profiles
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Brand, CreateBrandDto>().ReverseMap();
+            CreateMap<Brand, CreateBrandCommand>().ReverseMap();
+        }
+    }
+}
